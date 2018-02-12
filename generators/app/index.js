f2cc3fa46a18bc69662308ce0309e52c7b4d9579
Dmitriy Sharshakov aka. sh7dm <sh7dm@outlook.com>
2018-02-12 15:32:52 +0300

    Fixed syntax errors

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,16 +6,18 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the prime ' + chalk.red('generator-node-next') + ' generator!'
-    ));
+    this.log(
+      yosay('Welcome to the prime ' + chalk.red('generator-node-next') + ' generator!')
+    );
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to enable this option?',
+        default: true
+      }
+    ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
