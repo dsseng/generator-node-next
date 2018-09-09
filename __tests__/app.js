f89f7bbdea1251284fd3daf6babec903ff8dfd8e
sh7dm <sh7dm@outlook.com>
2018-09-09 20:02:45 +0300

    test: refactor tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -2,26 +2,28 @@
 const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
+const generatorPath = path.join(__dirname, '../generators/app');
 
 describe('generator-node-next:app', () => {
-  beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({});
-  });
-
   it('creates files', () => {
-    assert.file([
-      'package.json',
-      '.eslintrc',
-      '.eslintignore',
-      '.gitignore',
-      'README.md',
-      '.babelrc',
-      '.editorconfig',
-      '.travis.yml',
-      'src/index.js',
-      'src/sum.js',
-      '__tests__/sum.spec.js',
-      'Dockerfile'
-    ]);
+    return helpers
+      .run(generatorPath)
+      .withPrompts({})
+      .then(function() {
+        assert.file([
+          'package.json',
+          '.eslintrc',
+          '.eslintignore',
+          '.gitignore',
+          'README.md',
+          '.babelrc',
+          '.editorconfig',
+          '.travis.yml',
+          'src/index.js',
+          'src/sum.js',
+          '__tests__/sum.spec.js',
+          'Dockerfile'
+        ]);
+      });
   });
 });
