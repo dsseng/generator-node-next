2c7e9c269155b5a85eb6193a9833b280089ee1ab
sh7dm <d3dx12.xx@gmail.com>
2018-11-27 19:04:02 +0300

    docs: add GitHub links in docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,10 +12,26 @@ module.exports = {
         collapsable: true,
         children: [
           '/guides/backpack',
-          '/guides/docker'
+          '/guides/docker',
+          '/guides/ava',
+          '/guides/jest'
         ]
       }
-    ]
+    ],
+    // Assumes GitHub. Can also be a full GitLab url.
+    repo: 'sh7dm/generator-node-next',
+    // Customising the header label
+    // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
+    repoLabel: 'Contribute!',
+
+    // if your docs are not at the root of the repo:
+    docsDir: 'docs',
+    // if your docs are in a specific branch (defaults to 'master'):
+    docsBranch: 'docs/vuepress',
+    // defaults to false, set to true to enable
+    editLinks: true,
+    // custom text for edit link. Defaults to "Edit this page"
+    editLinkText: 'Help us improve this page!'
   },
   head: [
     ['link', { rel: 'shortcut icon', type: "image/png", href: `/icon.png` }]
